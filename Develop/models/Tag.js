6c21552396ce39826b59153f9b7e2656126cbf56
rubiocode <78938193+rubiocode@users.noreply.github.com>
2021-07-29 14:31:21 -0700

    created id and tag_id code for tag.js

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -1,12 +1,24 @@
+// import important parts from sequelize
 const { Model, DataTypes } = require('sequelize');
 
+// import our database connection from config.js
 const sequelize = require('../config/connection.js');
 
+// Initialize Tag model (table) by extending off Sequelize's Model class
 class Tag extends Model { }
 
+// set up fields and rules for Tag model
 Tag.init(
     {
-        // define columns
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        tag_name: {
+            type: DataTypes.STRING,
+        }
     },
     {
         sequelize,
